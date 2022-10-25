1f69ce2857fb61d6a0b7b9525f74a42f7612db3c
kanuos <sounakmukherjee@ymail.com>
2022-10-25 15:21:13 -0400

    added site colors to tailwind as per style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["*.html"],
   theme: {
-    fontFamily : {
-      "sans" : "Inter",
+    fontFamily: {
+      sans: "Inter",
     },
-    extend: {},
+    colors: {
+      "soft-orange": "hsl(35, 77%, 62%)",
+      "soft-red": "hsl(5, 85%, 63%)",
+      "off-white": "hsl(36, 100%, 99%)",
+      "grayish-blue": "hsl(233, 8%, 79%)",
+      "dark-grayish-blue": "hsl(236, 13%, 42%)",
+      "very-dark-blue": "hsl(240, 100%, 5%)",
+    },
   },
   plugins: [],
-}
+};
