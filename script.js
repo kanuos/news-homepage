2274d048e3ed8fd28d7b2f34e32d2ae1e3e64fa7
kanuos <sounakmukherjee@ymail.com>
2022-10-26 10:44:31 -0400

    toggle navbar visibility when page is scrolled

--- script.js
+++ script.js
@@ -1,15 +1,48 @@
 // DOM elements
+const navBar = document.querySelector("nav");
 const navhambuergerBtn = document.getElementById("openBtn");
 const navCloseBtn = document.getElementById("closeBtn");
 const navMenu = document.getElementById("navMenu");
 
+// variables & constants
+let currentScrollDistance = 0;
+const navbarClasses = ["drop-shadow-xl", "sticky", "top-0"];
+
 // functions
+
+/**
+ * @desc
+ This function hides/shows the nav menu on mobile and tablet views.
+ To hide the nav menu : the translate-x-full tailwind class is added to the menu
+ To show the nav menu : the translate-x-full tailwind class is removed to the menu
+ Translate-x-full as the name suggests performs the "transform : translateX(100%)"
+ Removing the transform class resets the nav-menu to its default X position i.e 0%
+ */
 function toggleNavMenu() {
   if (!navMenu) return;
-  console.log("clicked");
   navMenu.classList.toggle("translate-x-full");
+}
+
+/**
+ * @desc the navbar isn't made sticky by default which promotes a good UX. However, when
+ * the user has scrolled to the middle or the end of the page -- the user might want to access the nav menu
+ * to do so, when the page has been scrolled, the user can scroll up once to see the navbar which again disappears
+ * on scrolling down
+ */
+function toggleNavBarVisibility() {
+  // variable to detect whether user scrolled down or up the page
+  const scrollDirection =
+    currentScrollDistance > window.scrollY ? "up" : "down";
+  currentScrollDistance = window.scrollY;
+
+  if (scrollDirection === "down" || currentScrollDistance === 0) {
+    navBar?.classList.remove(...navbarClasses);
+    return;
+  }
+  navBar?.classList.add(...navbarClasses);
 }
 
 // Event listeners
 navhambuergerBtn?.addEventListener("click", toggleNavMenu);
 navCloseBtn?.addEventListener("click", toggleNavMenu);
+document?.addEventListener("scroll", toggleNavBarVisibility);
